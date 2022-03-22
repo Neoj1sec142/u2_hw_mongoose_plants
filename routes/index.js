85809a6ee2d2e37322391d5f6e2ffb7390482d98
Mark Harmon <neothej1sec@gmail.com>
2022-03-22 15:18:42 -0500

    up to graballplants controller

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ const { Router } = require('express');
 const controllers = require('../controllers')
 const router = Router();
 
+
 router.get('/', (req, res) => res.send('This is root!'))
 
 router.post('/plants', controllers.createPlant)
+
+router.get('/plants', controllers.getAllPlants)
 
 module.exports = router;
